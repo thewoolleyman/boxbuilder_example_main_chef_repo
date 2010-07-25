73f80800d4265d24407f6c22b7cfb2750c86da52
Chad Woolley <thewoolleyman@gmail.com>
2010-07-25 02:19:12 -0700

    just hardcode chef dir to ~/.chef

--- config/solo.rb
+++ config/solo.rb
@@ -1,4 +1,4 @@
-chef_dir ||= ENV["BOXBUILDER_CHEF_DIR"] ||= ENV["CHEF_DIR"] || File.expand_path('../..',__FILE__)
+chef_dir = File.expand_path('.chef',ENV['HOME'])
 file_cache_path chef_dir
 file_store_path chef_dir
 log_level :info
