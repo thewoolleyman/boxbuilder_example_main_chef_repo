596434fa134cfc52e1e124fbbcd0f125626661b7
Chad Woolley <thewoolleyman@gmail.com>
2010-07-25 22:43:22 -0700

    add references to example2 cookbook

--- config/solo.rb
+++ config/solo.rb
@@ -5,5 +5,5 @@ log_level :debug
 Chef::Log::Formatter.show_time = false
 cookbook_path [
   File.expand_path('../../site-cookbooks',__FILE__),
-  # File.expand_path('boxbuilder_example2_chef_repo/site-cookbooks',chef_dir)
+  File.expand_path('boxbuilder_example2_chef_repo/site-cookbooks',chef_dir)
 ]
