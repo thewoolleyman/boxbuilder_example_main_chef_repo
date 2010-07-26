4f8d18223f28830a9126c6a68854f227dd0df3a3
Chad Woolley <thewoolleyman@gmail.com>
2010-07-25 20:41:09 -0700

    fix paths in config

--- config/solo.rb
+++ config/solo.rb
@@ -1,8 +1,9 @@
-chef_dir = File.expand_path('../..',__FILE__)
+chef_dir = File.expand_path('../../..',__FILE__)
 file_cache_path chef_dir
 file_store_path chef_dir
 log_level :info
 Chef::Log::Formatter.show_time = false
 cookbook_path [
-  File.join(chef_dir,"boxbuilder_example1_chef_repo/site-cookbooks")
+  File.expand_path('../../site-cookbooks',__FILE__),
+  File.expand_path('boxbuilder_example2_chef_repo/site-cookbooks',chef_dir)
 ]
