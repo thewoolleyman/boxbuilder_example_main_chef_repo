5e58323379ccfda36737cbc3753c823d6b13d950
Chad Woolley <thewoolleyman@gmail.com>
2010-07-25 22:39:50 -0700

    add references to example2 cookbook

--- site-cookbooks/boxbuilder_example1_cookbook/recipes/default.rb
+++ site-cookbooks/boxbuilder_example1_cookbook/recipes/default.rb
@@ -1,3 +1,12 @@
-bash "example1 touchfile" do
-  code "touch $HOME/.tmp_boxbuilder_example1_chef_cookbook.touchfile"
+touchfile = "#{ENV['HOME']}/.tmp_boxbuilder_example1_chef_cookbook.touchfile"
+
+file touchfile do
+  owner "#{ENV['USER']}"
+  group "#{ENV['USER']}"
+  mode "0644"
+  action :create
+end
+
+bash "Write property value to file" do
+  code "echo '#{node['boxbuilder_example1_chef_config_key']}' > #{touchfile}"
 end
