8ad6071df4085ec04faa541b6303c7309eec9258
Chad Woolley <thewoolleyman@gmail.com>
2010-07-25 20:45:17 -0700

    use debug as default log level for now

--- config/solo.rb
+++ config/solo.rb
@@ -1,7 +1,7 @@
 chef_dir = File.expand_path('../../..',__FILE__)
 file_cache_path chef_dir
 file_store_path chef_dir
-log_level :info
+log_level :debug
 Chef::Log::Formatter.show_time = false
 cookbook_path [
   File.expand_path('../../site-cookbooks',__FILE__),
