9280041e41c17c89ad42361da20fa0f74cb9011c
Chad Woolley <thewoolleyman@gmail.com>
2010-08-31 01:25:47 -0700

    moved config to separate repo

--- config/solo.rb
+++
@@ -1,9 +0,0 @@
-chef_dir = File.expand_path('../../..',__FILE__)
-file_cache_path chef_dir
-file_store_path chef_dir
-log_level :debug
-Chef::Log::Formatter.show_time = false
-cookbook_path [
-  File.expand_path('../../site-cookbooks',__FILE__),
-  File.expand_path('boxbuilder_example2_chef_repo/site-cookbooks',chef_dir)
-]
