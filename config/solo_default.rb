4eb929cd86bee69b85c30609b51a78c1bd021d2a
Chad Woolley <thewoolleyman@gmail.com>
2010-09-11 17:16:22 -0700

    fix name typo, dependency, not dependent

--- config/solo_default.rb
+++ config/solo_default.rb
@@ -5,5 +5,5 @@ log_level :debug
 Chef::Log::Formatter.show_time = false
 cookbook_path [
   File.expand_path('boxbuilder_example_main_chef_repo/cookbooks',chef_dir),
-  File.expand_path('boxbuilder_example_dependent_chef_repo/cookbooks',chef_dir)
+  File.expand_path('boxbuilder_example_dependency_chef_repo/cookbooks',chef_dir)
 ]
