f12279edfab83c44f5a91a3716e2f679c0186dca
Chad Woolley <thewoolleyman@gmail.com>
2010-09-11 18:11:48 -0700

    move attributes from json to attributes file

---
+++ cookbooks/boxbuilder_example_main_cookbook/attributes/touchfile.rb
@@ -0,0 +1,2 @@
+node.default["boxbuilder_example_main_chef_config_key"] = "boxbuilder_example_main_chef_config_value"
+node.default["boxbuilder_example_dependency_chef_config_key"] = "boxbuilder_example_dependency_chef_config_set_from_main_value"
