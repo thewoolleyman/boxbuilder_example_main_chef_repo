bcd0e7ca5499a49bccdaf17108ea645e616a469e
Chad Woolley <thewoolleyman@gmail.com>
2010-09-11 18:39:56 -0700

    try override

--- cookbooks/boxbuilder_example_main_cookbook/attributes/touchfile.rb
+++ cookbooks/boxbuilder_example_main_cookbook/attributes/touchfile.rb
@@ -1,3 +1,2 @@
 node.default["boxbuilder_example_main_chef_config_key"] = "boxbuilder_example_main_chef_config_value"
-# include_attribute "boxbuilder_example_dependency_cookbook::touchfile" # didn't work
-node.default["boxbuilder_example_dependency_chef_config_key"] = "boxbuilder_example_dependency_chef_config_set_from_main_value"
+node.override["boxbuilder_example_dependency_chef_config_key"] = "boxbuilder_example_dependency_chef_config_set_from_main_value"
