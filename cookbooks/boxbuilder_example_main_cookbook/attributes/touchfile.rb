bb21c80227878348733c847d175cff8283fb7688
Chad Woolley <thewoolleyman@gmail.com>
2010-09-11 18:30:59 -0700

    commit non-working include_attribute try

--- cookbooks/boxbuilder_example_main_cookbook/attributes/touchfile.rb
+++ cookbooks/boxbuilder_example_main_cookbook/attributes/touchfile.rb
@@ -1,2 +1,3 @@
 node.default["boxbuilder_example_main_chef_config_key"] = "boxbuilder_example_main_chef_config_value"
+# include_attribute "boxbuilder_example_dependency_cookbook::touchfile" # didn't work
 node.default["boxbuilder_example_dependency_chef_config_key"] = "boxbuilder_example_dependency_chef_config_set_from_main_value"
